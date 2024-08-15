89b3f7f10ac1646db270bed4dcaf77465d58a53f
ABDULS21985 <sabdul@cbn.gov.ng>
2024-08-15 15:56:06 +0100

    "Set up Kafka and Zookeeper with Docker Compose, configured KafkaJS consumer in NestJS service, and resolved leadership election and group coordination issues."

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,15 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { KafkaModule } from './kafka/kafka.module';
+import { KafkaConsumerService } from './kafka/kafka.service';
+import { PrismaService } from './prisma/prisma.service';
 
 
 
 @Module({
   imports: [PrismaModule, KafkaModule,],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, KafkaConsumerService, PrismaService],
 })
 export class AppModule {}
+
